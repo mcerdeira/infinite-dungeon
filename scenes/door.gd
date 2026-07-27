83c628a02c53b6e231225a3f807aef4ad046d8ee
stinchi <martincerdeira@gmail.com>
2026-07-26 22:44:26 -0300

    algo de los portales

--- scenes/door.gd
+++ scenes/door.gd
@@ -1,17 +1,26 @@
-extends StaticBody2D
+extends Area2D
 var first = true
 
-func _physics_process(delta: float) -> void:
-	if first:
-		first = false
-		var enemies = get_tree().get_nodes_in_group("enemies")
-		if enemies.size() <= 0:
-			$Timer.start()
-
-func open():
+func _ready() -> void:
+	add_to_group("doors")
+	visible = false
 	$collider.set_deferred("disabled", true)
-	$AnimationPlayer.play("new_animation")
 
-func _on_timer_timeout() -> void:
-	$Timer.stop()
-	open()
+func close():
+	visible = true
+	$collider.set_deferred("disabled", false)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body and body.is_in_group("players"):
+		var width = get_viewport().get_visible_rect().size.x
+		var height = get_viewport().get_visible_rect().size.y
+		var player_postition = Global.player_obj.global_position
+		
+		if player_postition.x > width - 64:
+			Global.player_obj.global_position.x = 64
+		elif player_postition.x < 64:
+			Global.player_obj.global_position.x = width - 64
+		elif player_postition.y > height - 64:
+			Global.player_obj.global_position.y = 64
+		elif player_postition.y < 64:
+			Global.player_obj.global_position.y = height - 64
--- scenes/rooms/room.gd
+++ scenes/rooms/room.gd
@@ -1,4 +1,20 @@
 extends TileMapLayer
+var first = true
+var doors = []
 
 func _ready() -> void:
 	add_to_group("rooms")
+	
+func _physics_process(delta: float) -> void:
+	if first:
+		doors = get_tree().get_nodes_in_group("doors")
+		first = false
+		var enemies = get_tree().get_nodes_in_group("enemies")
+		if enemies.size() + 1 > 0:
+			$Timer.start()
+
+func _on_timer_timeout() -> void:
+	for d in doors:
+		if d and is_instance_valid(d):
+			d.close()
+			$Timer.stop()
